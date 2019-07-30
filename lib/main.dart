4b786f745d85f91568ea6b777cbcd420aaec9de1
Suchy <marcinsuchora@gmail.com>
2019-07-31 00:45:15 +0200

    Trying to display sprite

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flame/components/component.dart';
 import 'package:flame/game.dart';
 import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
@@ -7,16 +8,35 @@ void main() => runApp(
 );
 
 class MyGame extends Game{
+  Enemy enemy;
   @override
+
+  MyGame(){
+    enemy=new Enemy();
+    enemy.x=200;
+    enemy.y=200;
+
+  }
   void render(Canvas canvas) {
-    var rect= new Rect.fromLTWH(100, 100, 100, 10);
+    var rect= new Rect.fromLTWH(0, 0, 10, 10);
     var paint = new Paint();
     paint.color = new Color(0xFFFF0000);
+    //canvas.drawPaint(paint);
+    var img;
+    img=Flame.images.load("enemy.jpg");
+
+    //var text=new Text(10,10);
     canvas.drawRect(rect, paint);
+    canvas.drawImage(img,Offset(10, 10),paint);
   }
 
   @override
   void update(double t) {
     // TODO: implement update
+  }
+}
+class Enemy extends SpriteComponent{
+  Enemy():super.square(128.0,'enemy.jpg'){
+    this.angle=0.0;
   }
 }
