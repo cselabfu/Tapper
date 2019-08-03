7c00fab2f6f21e393d300d5d747d3f809a8e818e
Suchy <marcinsuchora@gmail.com>
2019-08-04 01:22:50 +0200

    Added list of enemies

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
+import 'dart:ui';
+
 import 'package:flame/components/component.dart';
 import 'package:flame/game.dart';
 import 'package:flame/flame.dart';
-import 'package:flame/palette.dart';
-import 'package:flame/position.dart';
 import 'package:flame/sprite.dart';
-import 'package:flame/text_config.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 
@@ -15,48 +13,56 @@ void main() async{
 }
 
 class MyGame extends Game{
-  Enemy enemy;
-  Sprite sprite;
+  List<Enemy> enemies=[];
   Size screenSize;
   Rect emmemy;
   @override
 
   MyGame(){
-    enemy=new Enemy();
-    enemy.x=200;
-    enemy.y=200;
-    sprite=new Sprite('enemy.png');
+    enemies.add(new Enemy());
     emmemy=Rect.fromLTWH(100, 100, 30, 30);
   }
   void render(Canvas canvas) {
     var rect= new Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
     var paint = new Paint();
-    paint.color = new Color(0xFFFF0000);
-    //canvas.drawPaint(paint);
-    //var img;
-    //img=Flame.images.load("enemy.png");
-
-    //sprite.render(canvas,100,100);
-    TextConfig textConfig=TextConfig(fontSize: 30,color: BasicPalette.white.color,fontFamily: 'Arial',textAlign: TextAlign.center);
-    //var text=new Text(10,10);
+    paint.color = new Color(0xF00F0F30);
     canvas.drawRect(rect, paint);
-    textConfig.render(canvas, "XDXDXDXD", Position(10,10));
-    sprite.renderRect(canvas, emmemy);
-    //canvas.drawImage(img,Offset(10, 10),paint);
+    enemies.first.render(canvas);
+    //sprite.renderRect(canvas, emmemy);
   }
 
   @override
   void update(double t) {
-    // TODO: implement update
+    enemies.forEach((Enemy)=>Enemy.update(t));
+    //if(enemy.y>screenSize.height)enemy.y=-100;
+    //window.onPointerDataPacket = (packet){
+      //var pointer = packet.data.first;
+      //MyGame.input();
+    //}
   }
   void resize(Size size) {
     screenSize = size;
     super.resize(size);
   }
+
+  void input(double x, double y) {
+
+  }
+
 }
-class Enemy extends SpriteComponent{
-  Enemy():super.square(128.0,'enemy.png'){
+class Enemy extends SpriteComponent {
+
+  Enemy():super.square(100,'enemy.png'){
     this.angle=0.0;
+    this.y=100;
+  }
+  @override
+  void update(double t) {
+    super.update(t);
+    y+=t*100;
+    if(y>300){
+      y=0;
+    }
   }
 }
 
