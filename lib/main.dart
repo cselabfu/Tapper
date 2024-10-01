33fae9be785a8b1dfff248e71ce6f478c1c1d956
cselabfu <cselab.fu@gmail.com>
2024-10-01 12:13:48 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,9 @@ void main() {
 
 class MyGame extends FlameGame with TapDetector {
   int points = 0;
-  int enemySpeed = 100;
-  double increaseCounter = 0.0;
+  int enemySpeed = 75, puppySpeed = 100;
+  double enemyIncreaseCounter = 0.0;
+  double puppyIncreaseCounter = 0.0;
   late TextComponent pointsText;
 
   @override
@@ -47,6 +48,8 @@ class MyGame extends FlameGame with TapDetector {
 
     // Add the initial enemy
     createEnemy(enemySpeed);
+    // Add the initial puppy
+    createPuppy(puppySpeed);
   }
 
   @override
@@ -54,11 +57,28 @@ class MyGame extends FlameGame with TapDetector {
     super.update(dt);
 
     // Update the enemy speed and spawn rate
-    increaseCounter += dt;
-    if (increaseCounter >= 0.4) {
-      enemySpeed += 45;
-      increaseCounter = 0;
+    enemyIncreaseCounter += dt;
+    // print(enemyIncreaseCounter);
+    if (enemyIncreaseCounter >= 0.8) {
+      // enemySpeed += 10;
+      if (points % 10 == 0) {
+        enemySpeed += points ~/ 10;
+      }
+
+      enemyIncreaseCounter = 0;
       createEnemy(enemySpeed);
+    }
+
+    // Update the enemy speed and spawn rate
+    puppyIncreaseCounter += dt;
+    if (puppyIncreaseCounter >= 0.5) {
+      // puppySpeed += 10;
+      if (points % 10 == 0) {
+        puppySpeed += points ~/ 10;
+      }
+
+      puppyIncreaseCounter = 0;
+      createPuppy(puppySpeed);
     }
 
     // Update the points display
@@ -69,6 +89,11 @@ class MyGame extends FlameGame with TapDetector {
   void createEnemy(int speed) {
     final enemy = Enemy(speed);
     add(enemy);
+  }
+
+  void createPuppy(int speed) {
+    final puppy = Puppy(speed);
+    add(puppy);
   }
 
   @override
@@ -86,6 +111,16 @@ class MyGame extends FlameGame with TapDetector {
 
     for (var enemy in enemiesToRemove) {
       enemy.removeFromParent(); // Removes the enemy from the game
+      points--; // decrement points
+    }
+
+    // Find all enemies that contain the tap position
+    final puppiesToRemove = children.whereType<Puppy>().where((puppy) {
+      return puppy.collisionCheck(x, y);
+    }).toList();
+
+    for (var puppy in puppiesToRemove) {
+      puppy.removeFromParent(); // Removes the puppy from the game
       points++; // Increment points
     }
   }
@@ -94,12 +129,47 @@ class MyGame extends FlameGame with TapDetector {
 class Enemy extends SpriteComponent with HasGameRef<MyGame> {
   final int speed;
 
-  Enemy(this.speed) : super(size: Vector2.all(30));
+  Enemy(this.speed) : super(size: Vector2.all(75));
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
     sprite = await gameRef.loadSprite('enemy.png');
+
+    // Position the enemy at a random x within the screen bounds and above the visible area
+    position = Vector2(
+      Random().nextDouble() * (gameRef.size.x - size.x),
+      -size.y,
+    );
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    position.y += dt * speed;
+
+    // Remove the enemy if it moves below the screen
+    if (position.y > gameRef.size.y) {
+      removeFromParent();
+    }
+  }
+
+  /// Checks if the given (x, y) coordinates collide with this enemy
+  bool collisionCheck(double x, double y) {
+    return Rect.fromLTWH(position.x, position.y, size.x, size.y)
+        .contains(Offset(x, y));
+  }
+}
+
+class Puppy extends SpriteComponent with HasGameRef<MyGame> {
+  final int speed;
+
+  Puppy(this.speed) : super(size: Vector2.all(75));
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    sprite = await gameRef.loadSprite('dog01.png');
 
     // Position the enemy at a random x within the screen bounds and above the visible area
     position = Vector2(
